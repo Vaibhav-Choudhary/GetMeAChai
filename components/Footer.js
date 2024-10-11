3bdc39044a2c00fe8a24044f6ba3394408a9f1a4
Vaibhav-Choudhary <sunitchoudhary32@gmail.com>
2024-10-11 23:47:19 +0530

    NavBar and Footer Updated

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=" items-center h-16 bg-black text-white flex justify-center px-4">
+        <p className='text-center'>Copyright &copy; Get me a Chai - Fund your projects with chai - All roghts Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
